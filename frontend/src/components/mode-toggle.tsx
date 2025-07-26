a26f86618754baddc7e4595ef6e1a0c3b56a29df
partiksingh1 <20btrcs105@jainuniversity.ac.in>
2025-07-26 15:38:50 +0200

    bug : linting issue fix

--- frontend/src/components/mode-toggle.tsx
+++ frontend/src/components/mode-toggle.tsx
@@ -1,6 +1,6 @@
 import { Moon, Sun } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import { useTheme } from "@/components/theme-provider"
+import { useTheme } from "@/lib/use-theme"
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme()
